f89e86b4bacce7ffebd5699516262fbe0b3a0fac
Klevertton dos Santos <klevertton.kw@hotmail.com>
2025-02-02 19:37:36 -0300

    Deploy of the base layout version - Without responsiveness

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  autoplay: false,
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  variableWidth: false,
+  prevArrow: $('.slick-prev'), // Define a seta de voltar
+  nextArrow: $('.slick-next')
+});
